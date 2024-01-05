6b7c4d2af69817460781cde9c9909689e2e4dd41
trislee02 <quangtri2002lk@gmail.com>
2024-01-05 14:52:58 +0700

    first commit

---
+++ Assets/Scripts/IKFootPlacement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKFootPlacement : MonoBehaviour
+{
+    Animator anim;
+
+    [Range(0f, 1f)]
+    [SerializeField]
+    private float distanceToGround;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, anim.GetFloat("IKLeftFootWeight"));
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, anim.GetFloat("IKLeftFootWeight"));
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, anim.GetFloat("IKRightFootWeight"));
+        anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, anim.GetFloat("IKRightFootWeight"));
+
+        // Left foot
+        RaycastHit hit;
+        Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+        if (Physics.Raycast(ray, out hit, distanceToGround + 1f, layerMask))
+        {
+            if (hit.transform.tag.Equals("Walkable"))
+            {
+                Vector3 footPosition = hit.point;
+                footPosition.y += distanceToGround;
+                anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+                anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+            }
+        }
+
+        // Right foot
+        ray = new Ray(anim.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
+        if (Physics.Raycast(ray, out hit, distanceToGround + 1f, layerMask))
+        {
+            if (hit.transform.tag.Equals("Walkable"))
+            {
+                Vector3 footPosition = hit.point;
+                footPosition.y += distanceToGround;
+                anim.SetIKPosition(AvatarIKGoal.RightFoot, footPosition);
+                anim.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float jumpSpeed;
+    public float jumpButtonGracePeriod;
+
+    private Animator animator;
+    private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            animator.SetBool("isMoving", true);
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
