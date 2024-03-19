b2a46deb9cafc7de7c011eb5da1f31bd4944ae3b
Le Quang Tri <quangtri2002lk@gmail.com>
2024-03-19 08:07:45 +0700

    Add waterfall and handrail

--- Assets/SpiralStair.cs
+++ Assets/SpiralStair.cs
@@ -14,6 +14,7 @@ public class SpiralStair : MonoBehaviour
     public bool isWallVisible = true;
     public GameObject wallCylinder;
     public GameObject stairStepObject;
+    public GameObject handrail;
 
     private Vector3 pivotPoint;
     private GameObject wall;
@@ -36,14 +37,31 @@ public class SpiralStair : MonoBehaviour
             else
                 stairStep = GameObject.CreatePrimitive(PrimitiveType.Cube);
             
-            Vector3 stepPosition = new Vector3(initialPosition.x, initialPosition.y + (i * rise) - (rise / 2), initialPosition.z);
+            Vector3 stepPosition = new Vector3(initialPosition.x, 
+                                               initialPosition.y + (i * rise) - (rise / 2), 
+                                               initialPosition.z);
+
             stepPosition = Quaternion.AngleAxis(angleTheta * i, startingPoint.up) * (stepPosition - pivotPoint) + pivotPoint;
             stairStep.SetActive(true);
             stairStep.transform.position = stepPosition;
-            stairStep.transform.localScale = new Vector3(treadLength + 2 * supplementTreadLength, rise, treadWidth + 0.1f);
+            stairStep.transform.localScale = new Vector3(treadLength + 2 * supplementTreadLength, 
+                                                         rise, 
+                                                         treadWidth + 0.1f); // +0.1f is the supplement width
+
             stairStep.transform.Rotate(stairStep.transform.up, angleTheta * i);
             stairStep.transform.parent = transform;
 
+            if (handrail != null)
+            {
+                float handrailHeight = 1.0f;
+                GameObject handrailInstance = Instantiate(handrail);
+                handrailInstance.transform.position = new Vector3(startingPoint.position.x + stairStep.transform.localScale.x / 2,
+                                                                  startingPoint.position.y + (i * rise) + (handrailHeight / 2),
+                                                                  startingPoint.position.z);
+                handrailInstance.transform.localScale = new Vector3(0.1f, handrailHeight, stairStep.transform.localScale.z);
+                handrailInstance.transform.RotateAround(pivotPoint, handrailInstance.transform.up, angleTheta * i);
+                handrailInstance.transform.parent = transform;
+            }
         }
 
         float wallHeight = numberOfSteps * rise;
@@ -54,8 +72,6 @@ public class SpiralStair : MonoBehaviour
         
         wall.transform.position = new Vector3(pivotPoint.x, pivotPoint.y + wallHeight/2.0f, pivotPoint.z);
         wall.transform.localScale = new Vector3(2*(radius+treadLength/2), wallHeight/2, 2* (radius + treadLength / 2));
-         
-
     }
 
     // Update is called once per frame
