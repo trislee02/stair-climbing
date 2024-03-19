8b2cfc7eded8ccc0626af127590a02798322ca21
loc4atnt <dorakid153@gmail.com>
2024-03-19 09:40:48 +0700

    add some texture for stair

--- Assets/Scripts/StairUp/Stair.cs
+++ Assets/Scripts/StairUp/Stair.cs
@@ -12,10 +12,14 @@ public class Stair : MonoBehaviour
     private GameObject stairWalls;
     public float wallHeight;
     public bool isWallVisible = true;
+    public Material stairMaterial;
+    public int wallStep = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 stairTextureScale = new Vector2(Mathf.Ceil(treadLength*3), Mathf.Ceil(treadWidth*3));
+
         stairWalls = new GameObject("StairWalls");
         for (int i = 1; i <= numberOfSteps; i++)
         {
@@ -25,18 +29,25 @@ public class Stair : MonoBehaviour
             stairStep.transform.localScale = new Vector3(treadLength, rise, treadWidth);
             stairStep.transform.parent = transform;
             stairStep.transform.localScale = new Vector3(treadLength, rise, treadWidth);
-            // Change the color of the cube to the random color
-            Color randomColor = new Color(
-                Random.Range(0f, 1f), // Red
-                Random.Range(0f, 1f), // Green
-                Random.Range(0f, 1f)  // Blue
-            );
-            stairStep.GetComponent<Renderer>().material.color = randomColor;
+            //// Change the color of the cube to the random color
+            //Color randomColor = new Color(
+            //    Random.Range(0f, 1f), // Red
+            //    Random.Range(0f, 1f), // Green
+            //    Random.Range(0f, 1f)  // Blue
+            //);
+            //stairStep.GetComponent<Renderer>().material.color = randomColor;
+            stairStep.GetComponent<Renderer>().material = stairMaterial;
+            stairStep.GetComponent<Renderer>().material.mainTextureScale = stairTextureScale;
+            stairStep.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
 
-            GameObject stairWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            stairWall.transform.position = new Vector3(startPosition.x + treadLength / 2.0f, startPosition.y + (i * rise) - (rise / 2) + (wallHeight / 2), startPosition.z + (i * treadWidth) - (treadWidth / 4));
-            stairWall.transform.localScale = new Vector3(0.1f, wallHeight, treadWidth);
-            stairWall.transform.parent = stairWalls.transform;
+            if (i % wallStep == 0)
+            {
+                GameObject stairWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //stairWall.transform.position = new Vector3(startPosition.x + treadLength / 2.0f, startPosition.y + ((i-4+1) * rise) - (rise / 2) + (wallHeight / 2), startPosition.z + ((i-4+1) * treadWidth) - (treadWidth / 2));
+                stairWall.transform.position = new Vector3(stepPosition.x + treadLength / 2f, stepPosition.y + wallHeight/2f - rise * wallStep, stepPosition.z - treadWidth * wallStep / 2f + (treadWidth/2f));
+                stairWall.transform.localScale = new Vector3(0.1f, wallHeight, treadWidth*wallStep);
+                stairWall.transform.parent = stairWalls.transform;
+            }
         }
     }
 
