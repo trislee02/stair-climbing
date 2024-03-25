b0ad18121994125edfa01a28eb2fecc673ca4586
Le Quang Tri <quangtri2002lk@gmail.com>
2024-03-25 11:02:14 +0700

    convert slope alpha input to step input

--- Assets/Scripts/Slope/Slope.cs
+++ Assets/Scripts/Slope/Slope.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Slope : MonoBehaviour
 {
+    public float stepWidth;
+    public float stepRise;
     public float length;
-    public float alpha;
     public float width;
-    public float segmentLength;
     public Vector3 startPosition;
     public float wallHeight;
     public float slopeThickness = 0.1f;
@@ -27,11 +27,11 @@ public class Slope : MonoBehaviour
         int envSampleLen = enviromentSamples.Length;
         Vector2 wallTextureScale = new Vector2(Mathf.Ceil(width * wallStep), Mathf.Ceil(wallHeight));
         Vector2 slopeTextureScale = new Vector2(Mathf.Ceil(width), Mathf.Ceil(length));
-        float rise = Mathf.Tan(alpha/180*Mathf.PI) * segmentLength;
-        float sinAlpha = Mathf.Sin(alpha / 180 * Mathf.PI);
-        float cosAlpha = Mathf.Cos(alpha / 180 * Mathf.PI);
+        float alpha = Mathf.Atan(stepRise / stepWidth);
+        float sinAlpha = Mathf.Sin(alpha);
+        float cosAlpha = Mathf.Cos(alpha);
 
-        Vector3 slopePosition = new Vector3(startPosition.x, startPosition.y + (sinAlpha * length/2) - slopeThickness - cosAlpha * slopeThickness, startPosition.z + (cosAlpha * length / 2) + rise);
+        Vector3 slopePosition = new Vector3(startPosition.x, startPosition.y + (sinAlpha * length/2) - slopeThickness - cosAlpha * slopeThickness, startPosition.z + (cosAlpha * length / 2) + stepRise);
         GameObject slopeObj;
         if (slopeSample != null)
         {
@@ -42,7 +42,7 @@ public class Slope : MonoBehaviour
             slopeObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
         }
         slopeObj.transform.position = slopePosition;
-        slopeObj.transform.Rotate(-alpha, 0, 0);
+        slopeObj.transform.Rotate(-alpha/Mathf.PI*180f, 0, 0);
         slopeObj.transform.localScale = new Vector3(width, slopeThickness, length);
         slopeObj.transform.parent = transform;
         slopeObj.GetComponent<Renderer>().material.mainTextureScale = slopeTextureScale;
@@ -59,7 +59,7 @@ public class Slope : MonoBehaviour
                 {
                     stairWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 }
-                stairWall.transform.position = new Vector3(startPosition.x + 1.9f + (i == wallStep ? 1.5f : 0.05f), startPosition.y + wallHeight / 2 + sinAlpha*(i-1) - rise, startPosition.z + cosAlpha * (i + 1 - wallStep));
+                stairWall.transform.position = new Vector3(startPosition.x + 1.9f + (i == wallStep ? 1.5f : 0.05f), startPosition.y + wallHeight / 2 + sinAlpha*(i-1) - stepRise, startPosition.z + cosAlpha * (i + 1 - wallStep));
                 stairWall.transform.localScale = new Vector3((i == wallStep ? 3f : 0.1f), wallHeight, cosAlpha * wallStep);
                 stairWall.transform.parent = transform;
                 //
@@ -69,7 +69,7 @@ public class Slope : MonoBehaviour
             if (handrailSample != null)
             {
                 GameObject handrailInstance = Instantiate(handrailSample);
-                handrailInstance.transform.position = new Vector3(startPosition.x - width / 2f, startPosition.y + handrailHeight / 2 + sinAlpha * (i - 1) - rise, startPosition.z + cosAlpha * (i));
+                handrailInstance.transform.position = new Vector3(startPosition.x - width / 2f, startPosition.y + handrailHeight / 2 + sinAlpha * (i - 1) - stepRise, startPosition.z + cosAlpha * (i));
                 handrailInstance.transform.localScale = new Vector3(0.1f, handrailHeight, cosAlpha * wallStep);
                 handrailInstance.transform.parent = transform;
             }
@@ -78,7 +78,7 @@ public class Slope : MonoBehaviour
             {
                 int rdIndex = Mathf.FloorToInt(Random.Range(0f, 0.99f) * envSampleLen);
                 GameObject envObj = Instantiate(enviromentSamples[rdIndex]);
-                envObj.transform.position = new Vector3(startPosition.x - 6.5f - width / 2f, startPosition.y + (sinAlpha * (i - 1) - rise) - Random.Range(1f, 5f), startPosition.z + cosAlpha * (i));
+                envObj.transform.position = new Vector3(startPosition.x - 6.5f - width / 2f, startPosition.y + (sinAlpha * (i - 1) - stepRise) - Random.Range(1f, 5f), startPosition.z + cosAlpha * (i));
                 envObj.transform.parent = transform;
             }
         }
