29d62bd159d45017d7ae2659c88763c6009ab1ac
Le Quang Tri <quangtri2002lk@gmail.com>
2024-06-30 15:14:39 +0700

    Add collectable object

--- Assets/Throwable.cs
+++ Assets/Throwable.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Throwable : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem effect;
+
+    private bool hasTouchTarget = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,28 +19,20 @@ public class Throwable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "snowman")
+        if (!hasTouchTarget && collision.gameObject.tag == "snowman")
         {
             Rigidbody rb = gameObject.GetComponent<Rigidbody>();
             rb.isKinematic = true;
-            //rb.velocity = Vector3.zero;
-            //rb.angularVelocity = Vector3.zero;
+            effect.transform.position = gameObject.transform.position;
+            effect.Play();
             Debug.Log("Collision enter");
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "snowman")
-        {
-            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-            rb.isKinematic = false;
-            Debug.Log("Collision exit");
+            hasTouchTarget = true;
+            //TODO: Add score
         }
     }
 
